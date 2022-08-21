9990d9994fcf4fca8e180435cff1c758e2280c05
Daniel Ortega Herrera <dhfilms11@gmail.com>
2022-08-21 13:21:14 -0500

    Este es el primer commit por la rama CarlosDaniel
    Este es el primer commit por la rama CarlosDaniel

--- repositorio.java
+++ repositorio.java
@@ -2,5 +2,6 @@ public class repositorio{
   public static void main(String args[]){
 
    System.out.println("Bienvenidos al repositorio Java-Start"); 
+   /*Este es el primer comentario escrito por la rama CarlosDaniel del proyecto Java-Start*/ 
  }
 }
